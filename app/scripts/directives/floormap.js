742a82a1d32062cc1a834ddcd77477a8fc829b97
ahlunju <ahlunju@hotmail.com>
2015-03-21 23:52:47 -0400

    removed red clone dot

--- app/scripts/directives/floormap.js
+++ app/scripts/directives/floormap.js
@@ -10,30 +10,17 @@ angular.module('projectSsApp')
   .directive('floorMap', function () {
 
   // constants
-	var width = 500;
-	var height = 500;
+	var width = 1000;
+	var height = 1000;
 	return {
 	restrict: 'E',
 	link: function (scope, element, attr) {
-			// console.log(attr);
-
-		function clone(obj){
-			if(obj == null || typeof(obj) != 'object')
-				return obj;
-
-			var temp = obj.constructor(); // changed
-
-			for(var key in obj) {
-				temp[key] = clone(obj[key]);
-				return temp;
-			}
-		}
 		// set up initial svg object
 		var vis = d3.selectAll(element)
 					.append("svg")
-					.attr("viewBox", "-10 -10 500 510")
+					.attr("viewBox", "-50 -50 1000 1000")
 					.attr("width", width)
-					.attr("height", height+20);
+					.attr("height", height);
 
 		var g = vis.selectAll('adding graphics element')
 					.data([{x:0, y:0}])
@@ -64,53 +51,32 @@ angular.module('projectSsApp')
 				.drag()
 				.on('dragstart', function() {
 					console.log(this);
-					var sel = d3.select(this),
-						cx = sel.attr('cx'),
-						cy = sel.attr('cy');
+					d3.select(this).classed("dragging", true);
 
-					var cursorData = d3.select(this).data();
-					console.log(this,cursorData);
-
-					g.selectAll('add drag memory')
-						.data(clone(cursorData)).enter()
-						.append('circle')
-						.classed('drag', true)
-						.attr('cx', cx)
-						.attr('cy', cy)
-						.attr('r', 3)
-						.attr('stroke', "black")
-						.attr('fill', "red");
 				})
 				.on('drag', function(d, i) {
-					var sel = d3.select('.drag'),
-						cy = sel.attr('cy'),
-						cx = sel.attr('cx');
-
-					sel.attr('cy', parseInt(cy)+d3.event.dy);
-					sel.attr('cx', parseInt(cx)+d3.event.dx);
-					d.v = height-Math.round(cy);
-					d.x = width-Math.round(cx);
+					d3.select(this)
+						.attr('cx', d.x = Math.round(d3.event.x))
+						.attr('cy', d.v = Math.round(d3.event.y));
 					scope.$apply();
-					console.log(d,i,cy, cx);
 				})
 				.on('dragend', function(d, i) {
-					var drag = d3.selectAll('.drag');
-					drag.remove();
+					d3.select(this).classed('drag', false);
 				});
 
 			g.selectAll('.points').call(dragfn);
 		};
 
 		scope.$watch(attr.ghBind, function(value){
-				console.log(value);
+				// console.log(value);
 				var changeFn = function(){
 					vis.selectAll(".points")
 						.data(value)
 						.attr("cy", function(d){
-							return height-d.v;
+							return d.v;
 						})
 						.attr('cx', function(d) {
-							return width-d.x;
+							return d.x;
 						});
 				}
 				if(isInitialsed){
