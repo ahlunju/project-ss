679ec89d25a676323ce93b44718b5cdaf269b97d
ahlunju <ahlunju@hotmail.com>
2015-04-06 12:29:22 -0400

    added sample shapes, grid lines

--- app/scripts/controllers/floorplanner.js
+++ app/scripts/controllers/floorplanner.js
@@ -8,6 +8,7 @@
  * Controller of the projectSsApp
  */
 angular.module('projectSsApp').controller('FloorplannerCtrl', function (floorPlanService, $scope) {
+	$scope.selectedEmployee = {};
 	// $scope.getEmployees(); // use local data for now
 	$scope.employees = [
 		{
@@ -36,4 +37,22 @@ angular.module('projectSsApp').controller('FloorplannerCtrl', function (floorPla
 			"deskID": 3
 		}
 	];
+
+	$scope.objects = {
+		"objects":[
+		{
+			"type":"rect","left":50,"top":50,"width":100,"height":50,
+			"fill":"green","strokeWidth":1,"angle":0,"selectable":true,
+			"hasControls":true,"hasBorders":true,"hasRotatingPoint":true,"transparentCorners":true,"perPixelTargetFind":false,"rx":0,"ry":0
+		},{
+			"type":"circle",
+			"left":500,"top":500,"width":100,"height":100,
+			"fill":"red","strokeWidth":1,"angle":0,"selectable":true,
+			"hasControls":true,"hasBorders":true,"hasRotatingPoint":true,"transparentCorners":true,"perPixelTargetFind":false,"radius":50
+		},{
+			"type":"rect","left":200,"top":50,"width":100,"height":50,
+			"fill":"orange","strokeWidth":1,"angle":90,"selectable":true,
+			"hasControls":true,"hasBorders":true,"hasRotatingPoint":true,"transparentCorners":true,"perPixelTargetFind":false,"rx":0,"ry":0
+		}]
+	};
 });
--- app/scripts/directives/floorcanvas.js
+++ app/scripts/directives/floorcanvas.js
@@ -11,13 +11,63 @@ return {
 	template: '<canvas id="floor-canvas"></canvas>',
 	restrict: 'E',
 	link: function postLink(scope, element, attrs) {
+
+		function draw_grid(gridSize) {
+			var w = canvas.width,
+				h = canvas.height;
+			/**
+			 * i is used for both x and y to draw
+			 * a line every 5 pixels starting at
+			 * .5 to offset the canvas edges
+			 */
+			for(var i = 0.5; i < w || i < h; i += gridSize) {
+				// draw horizontal lines
+				var horizontalLine = new fabric.Line([i, 0, i, h], {
+					stroke: '#000',
+					strokeWidth: 0.2,
+					selectable:false
+				});
+				var verticalLine = new fabric.Line([0, i, w, i], {
+					stroke: '#000',
+					strokeWidth: 0.2,
+					selectable:false
+				});
+				canvas.add(horizontalLine);
+				horizontalLine.sendToBack();
+				// draw vertical lines
+				canvas.add(verticalLine);
+				verticalLine.sendToBack();
+			}
+		}
+		
 		var canvas = new fabric.Canvas('floor-canvas', {
-			backgroundColor: 'rgb(100,100,200)',
-			selectionColor: 'blue',
-			selectionLineWidth: 2
-			// ...
+			backgroundColor: 'rgb(255,255,255)',
+			selectionColor: 'orange',
+			selectionLineWidth: 2,
+			width: 1500,
+			height: 800
 		});
-		scope.$apply();
+
+		
+		canvas.loadFromJSON(scope.objects);
+		draw_grid(10);
+		// var rect = new fabric.Rect({
+		//   left: 100,
+		//   top: 100,
+		//   fill: 'red',
+		//   width: 20,
+		//   height: 20
+		// });
+
+		// var circle = new fabric.Circle({
+		//   radius: 20, fill: 'green', left: 100, top: 100
+		// });
+		// var triangle = new fabric.Triangle({
+		//   width: 20, height: 30, fill: 'blue', left: 50, top: 50
+		// });
+
+		// canvas.add(circle, triangle);
+		// canvas.renderAll();
 	}
 };
 });
